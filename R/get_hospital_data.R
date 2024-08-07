008a004d0686ef71f14bd94b92f78ed12292fa59
Jones <t0jone12@louisville.edu>
2024-08-07 13:56:31 -0400

    Some updates

---
+++ R/get_hospital_data.R
@@ -0,0 +1,119 @@
+#' Create hospital dataset
+#'
+#' Function will identify all of the hospital datasets in your directory and create a dataset for them
+#'
+#' @param db_directory path to directory where data files are held
+#' @param hospital_files vector of names of hospital files
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+
+get_hospital_data <- function(
+    db_directory = ".",
+    hospital_files = NULL
+  ) {
+
+
+  # If there is no given vector of hospital files, prompt the users
+  if (is.null({{ hospital_files }})) {
+
+    # Get list of files in directory
+    files_in_directory <- list.files(path = db_directory)
+
+    hospital_files <- files_in_directory[grepl("hospital", files_in_directory)] |>
+      as.vector()
+
+    # Show files found and prompt user
+    cat(paste0("     ",
+               length(hospital_files),
+               " files found:"
+    )
+    )
+
+    cat("\n\n")
+
+    print(hospital_files)
+
+    cat("\n\n")
+
+    cat("     Unzip files to create dataset?")
+
+    cat("\n\n")
+
+    unzip_files_q <- readline(prompt = "yes/no: ")
+
+    # If yes then start going through and getting the data
+
+    if(tolower(unzip_files_q) %in% c("y", "yes", "ye")) {
+
+      # Print the loading bar
+      cat("\r[",
+          paste(rep("=", 0), collapse = ""),
+          paste(rep(" ", length(hospital_files) - 0), collapse = ""),
+          "] ",
+          0,
+          "/",
+          length(hospital_files),
+          " Files Processed",
+          sep = ""
+      )
+
+      flush.console()
+
+      results_list <- list()
+
+      for (i in 1:length(hospital_files)) {
+
+        current_file <- paste0(db_directory, hospital_files[i])
+
+        cat(current_file)
+
+        R.utils::gunzip(current_file,
+                        remove = FALSE)
+
+        unzip_current_file <- sub("\\.gz$", "", current_file)
+
+
+        read_data <- read_fwf(unzip_current_file,
+                         col_positions = fwf_positions(
+                           start = hospital_file_lbl$Start,
+                           end = hospital_file_lbl$Stop,
+                           col_names = hospital_file_lbl$name
+                         )
+        )
+
+        results_list[[current_file]] <- read_data
+
+        file.remove(unzip_current_file)
+
+        # Print the loading bar
+        cat("\r[",
+            paste(rep("=", i), collapse = ""),
+            paste(rep(" ", length(hospital_files) - i), collapse = ""),
+            "] ",
+            i,
+            "/",
+            length(hospital_files),
+            " Files Processed ",
+            sep = ""
+            )
+
+        flush.console()
+
+      }
+
+    } else {
+
+      cat("\nAll hospital files should include the word 'hospital'\n")
+
+    }
+
+  }
+
+  return(results_list)
+
+}
