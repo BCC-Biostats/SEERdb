d5d318c0d32abdfff0e4fa35a6fcb851fd8d257c
Jones <t0jone12@louisville.edu>
2024-08-12 19:10:17 -0400

    update data

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' Hospital File Labels.
-#'
-#' Guide for hospital files that include positions for values, column names and
-#' column labels
-#'
-#' @format A data frame with four variables: \code{Start}, \code{Stop},
-#'   \code{name}, \code{label}
-#'
-"hospital_file_labels"
