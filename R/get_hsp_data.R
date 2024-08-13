b11af6ffa9863c5ac6af0fb2ab4e33631af1ce71
Jones <t0jone12@louisville.edu>
2024-08-13 15:43:40 -0400

    Create function Tempaltes

---
+++ R/get_hsp_data.R
@@ -0,0 +1,26 @@
+#' Create hsp dataset
+#'
+#' Function will identify all of the hsp datasets in your directory and create a dataset for them
+#'
+#' @param db_directory path to directory where data files are held
+#' @param hospital_files vector of names of hospital files
+#'
+#' @return a list of dataframes from each given hospital file
+#' @export
+#'
+#' @examples
+#' hospitalDB <- SEERdb::get_hospital_data("../../master data files - SEER Medicare 2024/")
+#'
+#'
+#' hospitalDB <- SEERdb::get_hospital_data(
+#'                 "../../master data files - SEER Medicare 2024/",
+#'                 c("hospital1996.withzip.txt.gz", "hospital2000.withzip.txt.gz")
+#'                 )
+#'
+
+get_hsp_data <- function(
+    db_directory = ".",
+    hospital_files = NULL
+){
+
+}
---
+++ R/get_outpat_data.R
@@ -0,0 +1,26 @@
+#' Create outpat dataset
+#'
+#' Function will identify all of the outpat datasets in your directory and create a dataset for them
+#'
+#' @param db_directory path to directory where data files are held
+#' @param hospital_files vector of names of hospital files
+#'
+#' @return a list of dataframes from each given hospital file
+#' @export
+#'
+#' @examples
+#' hospitalDB <- SEERdb::get_hospital_data("../../master data files - SEER Medicare 2024/")
+#'
+#'
+#' hospitalDB <- SEERdb::get_hospital_data(
+#'                 "../../master data files - SEER Medicare 2024/",
+#'                 c("hospital1996.withzip.txt.gz", "hospital2000.withzip.txt.gz")
+#'                 )
+#'
+
+get_outpat_data <- function(
+    db_directory = ".",
+    hospital_files = NULL
+){
+
+}
