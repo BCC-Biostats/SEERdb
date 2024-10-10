bf8c499deb30dfcbc7e1e679275bc3390b848f64
Jones <t0jone12@louisville.edu>
2024-10-10 15:09:13 -0400

    Update Comorb function

--- R/comorb.R
+++ R/comorb.R
@@ -1,6 +1,7 @@
 #' Get Comorbidity Information
 #'
-#' This function processes claim data to identify comorbidities based on ICD-9 and ICD-10 diagnosis codes, and computes Charlson and NCI comorbidity indices.
+#' This function processes claim data to identify comorbidities based on ICD-9 and ICD-10 diagnosis codes,
+#' and computes Charlson and NCI comorbidity indices.
 #'
 #' @param infile A data frame containing claim records.
 #' @param id A character string specifying the column name for patient IDs.
@@ -34,96 +35,104 @@
 comorb <- function(infile, id, start_date, end_date, claim_start_date, claim_end_date,
                    claim_type, dx_var_list, ruleout = FALSE) {
 
-  # Load necessary libraries
   conditions <- c("acute_mi", "history_mi", "chf", "pvd", "cvd", "copd",
                   "dementia", "paralysis", "diabetes", "diabetes_comp",
                   "renal_disease", "mild_liver_disease", "liver_disease",
                   "ulcers", "rheum_disease", "aids")
 
-  # Step 1: Select records in the appropriate window
-  claims <- infile %>%
-    dplyr::select(dplyr::all_of(c(id, start_date, end_date, claim_start_date, claim_end_date, claim_type, dx_var_list)))
+  dx_code_list <- paste0("dx_code_", 1:length(dx_var_list))
 
-  if (ruleout) {
-    claims <- claims %>%
-      dplyr::filter((!!rlang::sym(start_date) - lubridate::days(30)) <= !!rlang::sym(claim_start_date) &
-                      !!rlang::sym(claim_start_date) <= (!!rlang::sym(end_date) + lubridate::days(30))) %>%
-      dplyr::mutate(inwindow = (!!rlang::sym(start_date) <= !!rlang::sym(claim_start_date) &
-                                  !!rlang::sym(claim_start_date) <= !!rlang::sym(end_date)))
-  } else {
-    claims <- claims %>%
-      dplyr::filter(!!rlang::sym(start_date) <= !!rlang::sym(claim_start_date) &
-                      !!rlang::sym(claim_start_date) <= !!rlang::sym(end_date)) %>%
-      dplyr::mutate(inwindow = TRUE)
-  }
+  dat <- infile |>
+    dplyr::select(dplyr::all_of(c(id,
+                                  claim_start_date,
+                                  claim_end_date,
+                                  dx_var_list))) |>
+    dplyr::rename_with(~ paste0("dx_code_", seq_along(.)), all_of(dx_var_list)) |>
+    dplyr::rename(patient_id = !!id,
+                  claim_start = !!claim_start_date,
+                  claim_end = !!claim_end_date) |>
+    dplyr::mutate(dx_codes = purrr::pmap(list(!!!rlang::syms(dx_code_list)), c)) |>
+    dplyr::select(-dplyr::starts_with("dx_code_")) |>
+    dplyr::mutate(claim_start = lubridate::ymd(claim_start),
+                  claim_end = lubridate::ymd(claim_end),
+                  icd_vrsn = ifelse(claim_end < as.Date("2015-10-1"), 9, 10)) |>
+    dplyr::mutate(acute_mi = purrr::map_lgl(dx_codes, ~ any(.x %in% c("410", "I21", "I22"))),
+                  history_mi = purrr::map_lgl(dx_codes, ~ any(.x %in% c("412", "I252"))),
+                  chf = purrr::map_lgl(dx_codes, ~ any(.x %in% c('39891','40201','40211','40291','40401','40403','40411',
+                                                                 '40413','40491','40493','428', '4254', '4255', '4256',
+                                                                 '4257', '4258', '4259'))),
+                  pvd = purrr::map_lgl(dx_codes, ~ any(.x %in% c('0930','440','441','4471','5571','5579','V434', '4431',
+                                                                 '4432', '4433', '4434' ,'4435', '4436', '4437', '4438', "4439"))),
+                  cvd = purrr::map_lgl(dx_codes, ~ any(.x %in% c('36234', '430', '431', '432', '433', '434', '435', '436',
+                                                                 '437', '438', 'G45','G46','H340','I6'))),
+                  copd = purrr::map_lgl(dx_codes, ~ any(.x %in% c('4168','4169','5064','5081','5088', '490', '490', '491',
+                                                                  '492', '493', '494', '495', '496', '497', '498', '499',
+                                                                  '500', '501', '502', '503', '504', '505', 'I278','I279',
+                                                                  'J684','J701','J703', 'J40', 'J41', 'J42', 'J43', 'J44',
+                                                                  'J45', 'J46', 'J47', 'J60', 'J61', 'J62', 'J63', 'J64',
+                                                                  'J65', 'J66', 'J67'))),
+                  dementia = purrr::map_lgl(dx_codes, ~ any(.x %in% c('290','2941','3312', 'F051','G30','G311',
+                                                                      'F00', 'F01','F02', 'F03'))),
+                  paralysis = purrr::map_lgl(dx_codes, ~ any(.x %in% c('3341','342','343','3449', '3440', '3441', '3442',
+                                                                       '3443', '3444', '3445', '3446'))),
+                  diabetes = purrr::map_lgl(dx_codes, ~ any(.x %in% c('E100','E101','E106','E108','E109','E110','E111','E116',
+                                                                      'E118','E119','E130','E131','E136','E138','E139',
+                                                                      '2500','2501','2502','2503','2508','2509'))),
+                  diabetes_comp = purrr::map_lgl(dx_codes, ~ any(.x %in% c('2504','2505','2506','2507', 'E102','E103','E104',
+                                                                           'E105','E107','E112','E113','E114','E115','E117',
+                                                                           'E132','E133','E134','E135','E137'))),
+                  renal_disease = purrr::map_lgl(dx_codes, ~ any(.x %in% c('40301','40311','40391','40402','40403','40412',
+                                                                           '40413','40492','40493','582','585','586','5880',
+                                                                           'V420','V451','V56', '5830', '5831','5832' ,'5833',
+                                                                           '5834','5835', '5836', '5837'))),
+                  mild_liver_disease = purrr::map_lgl(dx_codes, ~ any(.x %in% c('07022','07023','07032','07033','07044','07054','0706',
+                                                                                '0709','570','571','5733','5734','5738','5739','V427',
+                                                                                'B18','K709','K717','K73','K74','K760','K768','K769','Z944',
+                                                                                'K700', 'K701', 'K702','K703', 'K713', 'K714','K715',
+                                                                                'K762', 'K763', 'K764'))),
+                  liver_disease = purrr::map_lgl(dx_codes, ~ any(.x %in% c('I850','I859','I864','I982','K704','K711','K721','K729','K765','K766','K767',
+                                                                           '4560', '4561', '4562', '5722', '5723','5724' ,'5725' ,'5726' ,'5727' ,'5728'))),
+                  ulcers = purrr::map_lgl(dx_codes, ~ any(.x %in% c('531', '532','533' ,'534', 'K25', 'K26','K27','K28' ))),
+                  rheum_disease = purrr::map_lgl(dx_codes, ~ any(.x %in% c('M05','M06','M315','M32','M33','M34','M351','M353','M360',
+                                                                           '4465','7148','725', '7100', '7101', '7102','7103' ,'7104',
+                                                                           '7140', '7141', '7142'))),
+                  aids = purrr::map_lgl(dx_codes, ~ any(.x %in% c('B20','B21','B22','B24', '042', '043', '044')))
+    ) |>
+    dplyr::mutate(Charlson = 1*(acute_mi | history_mi) +
+                    1*(chf) +
+                    1*(pvd) +
+                    1*(cvd) +
+                    1*(copd) +
+                    1*(dementia) +
+                    2*(paralysis) +
+                    1*(diabetes & !diabetes_comp) +
+                    2*(diabetes_comp) +
+                    2*(renal_disease) +
+                    1*(mild_liver_disease &  !liver_disease) +
+                    3*(liver_disease) +
+                    1*(ulcers) +
+                    1*(rheum_disease) +
+                    6*(aids)) |>
+    dplyr::mutate(NCI_index = 0.12624*(acute_mi) +
+                    0.07999*(history_mi) +
+                    0.64441*(chf) +
+                    0.26232*(pvd) +
+                    0.27868*(cvd) +
+                    0.52487*(copd) +
+                    0.72219*(dementia) +
+                    0.39882*(paralysis) +
+                    0.29408*(diabetes | diabetes_comp) +
+                    0.47010*(renal_disease) +
+                    0.73803*(mild_liver_disease | liver_disease) +
+                    0.07506*(ulcers) +
+                    0.21905*(rheum_disease) +
+                    0.58362*(aids))
 
-  # Step 2: Determine ICD-9 or ICD-10 diagnosis (ICDVRSN)
-  claims <- claims %>%
-    dplyr::mutate(ICDVRSN = dplyr::case_when(
-      !!rlang::sym(claim_end_date) < lubridate::mdy("10-01-2015") ~ 9,
-      !!rlang::sym(claim_end_date) >= lubridate::mdy("10-01-2015") ~ 10
-    ))
 
-  # Step 3: Create comorbidity variables, initialized to 0
-  claims <- claims %>%
-    dplyr::mutate(acute_mi = 0, history_mi = 0, chf = 0, pvd = 0, cvd = 0,
-                  copd = 0, dementia = 0, paralysis = 0, diabetes = 0,
-                  diabetes_comp = 0, renal_disease = 0, mild_liver_disease = 0,
-                  liver_disease = 0, ulcers = 0, rheum_disease = 0, aids = 0)
+  dat <- dat |>
+    dplyr::filter(claim_start >= {{ start_date }},
+                  claim_end <= {{ end_date }})
 
-  # Step 4: Iterate over diagnosis codes and identify comorbidities
-  for (dx_var in dx_var_list) {
-    claims <- claims %>%
-      dplyr::mutate(dxcode = toupper(!!rlang::sym(dx_var))) %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(
-        acute_mi = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^410"), 1, acute_mi),
-        history_mi = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^412"), 1, history_mi),
-        chf = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^428"), 1, chf),
-        pvd = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^440|^441"), 1, pvd),
-        cvd = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^430|^438"), 1, cvd),
-        copd = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^490|^505"), 1, copd),
-        dementia = ifelse(ICDVRSN == 9 & stringr::str_detect(dxcode, "^290|^294"), 1, dementia),
-        # Additional comorbidities for ICD-10 codes
-        acute_mi = ifelse(ICDVRSN == 10 & stringr::str_detect(dxcode, "I21|I22"), 1, acute_mi),
-        history_mi = ifelse(ICDVRSN == 10 & stringr::str_detect(dxcode, "I252"), 1, history_mi),
-        chf = ifelse(ICDVRSN == 10 & stringr::str_detect(dxcode, "I50"), 1, chf)
-      )
-  }
 
-  # Step 5: Sort by id and claim_start_date
-  claims <- claims %>%
-    dplyr::arrange(!!rlang::sym(id), !!rlang::sym(claim_start_date))
-
-  # Step 6: Find first, last, and in-window dates for each condition
-  claims <- claims %>%
-    dplyr::group_by(!!rlang::sym(id)) %>%
-    dplyr::mutate(anyclaims = any(inwindow)) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(dplyr::across(dplyr::all_of(conditions), function(comorbidity) {
-      if (comorbidity == 1) {
-        # Logic to identify first, last claim dates
-        # Update variables like first_acute_mi_date, last_acute_mi_date, etc.
-      }
-      return(comorbidity)
-    }))
-
-  # Step 7: Calculate Charlson and NCI indices
-  claims <- claims %>%
-    dplyr::mutate(Charlson = 1 * (acute_mi | history_mi) + 1 * chf + 1 * pvd + 1 * cvd +
-                    1 * copd + 1 * dementia + 2 * paralysis + 1 * diabetes + 2 * diabetes_comp +
-                    2 * renal_disease + 1 * mild_liver_disease + 3 * liver_disease + 1 * ulcers +
-                    1 * rheum_disease + 6 * aids) %>%
-    dplyr::mutate(NCI_index = 0.12624 * acute_mi + 0.07999 * history_mi + 0.64441 * chf +
-                    0.26232 * pvd + 0.27868 * cvd + 0.52487 * copd + 0.72219 * dementia +
-                    0.39882 * paralysis + 0.29408 * (diabetes | diabetes_comp) +
-                    0.47010 * renal_disease + 0.73803 * (mild_liver_disease | liver_disease) +
-                    0.07506 * ulcers + 0.21905 * rheum_disease + 0.58362 * aids)
-
-  # Only keep subjects with claims in the window and return final dataset
-  claims <- claims %>%
-    dplyr::filter(anyclaims) %>%
-    dplyr::select(dplyr::all_of(c(id, start_date, end_date, paste0(conditions, "_date"), "Charlson", "NCI_index")))
-
-  return(claims)
+  return(dat)
 }
